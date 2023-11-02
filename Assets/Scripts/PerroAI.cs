ec5ce5cc5a9a65777c08e0f461b5b46cbcd881e7
AleHutmacher <91856761+AleHutmacher@users.noreply.github.com>
2023-11-02 00:57:57 -0300

    implementing new state machine

--- Assets/Scripts/PerroAI.cs
+++ Assets/Scripts/PerroAI.cs
@@ -2,17 +2,162 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum State
+{
+    Patrullando,
+    Persiguiendo,
+    Investigando,
+    Durmiendo,
+    Alerta
+}
+
 public class PerroAI : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public State currentState;
+    private Vector2 investigationTarget;
+    private Rigidbody2D rb;
+
+    // Variables para el patrullaje
+    public Transform[] patrolPoints;
+    private int currentPatrolIndex;
+    public float patrolSpeed;
+    public float chaseSpeed;
+
+    // Variables de detección del jugador
+    public float visionRange;
+    public float chaseThreshold;
+    private Transform player;
+
+    // Variable de tiempo de alerta e investigación
+    public float alertTime = 5f;
+    private float timer;
+
+    private bool sleeping = false;
+    public Animator anim;
+    private bool facingRight = true;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        currentState = State.Patrullando;
+        currentPatrolIndex = 0;
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        facingRight = true;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        switch (currentState)
+        {
+            case State.Patrullando:
+                Patrol();
+                CheckForPlayer();
+                break;
+            case State.Persiguiendo:
+                Chase();
+                CheckForPlayer();
+                break;
+            case State.Investigando:
+                Investigate();
+                break;
+            case State.Durmiendo:
+                // Lógica de dormir
+                break;
+            case State.Alerta:
+                Alert();
+                break;
+        }
+        checkFacing();
+    }
+
+    void checkFacing()
+    {
+        if (rb.velocity.x < 0 && facingRight)
+        {
+            Flip();
+        }
+        else if (rb.velocity.x > 0 && !facingRight)
+        {
+            Flip();
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+    void Sleep()
+    {
+        timer = 30f;
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            currentState = State.Patrullando;
+        }
+    }
+
+    void Patrol()
+    {
+        Vector2 target = patrolPoints[currentPatrolIndex].position;
+        Vector2 patrolDirection = (target - (Vector2)transform.position).normalized;
+        rb.velocity = patrolDirection * patrolSpeed;
+
+        if (Vector2.Distance(transform.position, target) < 1f)
+        {
+            currentPatrolIndex = (currentPatrolIndex + 1) % patrolPoints.Length;
+        }
+    }
+
+    void Chase()
+    {
+        if (Vector2.Distance(transform.position, player.position) > chaseThreshold)
+        {
+            currentState = State.Patrullando;
+            anim.SetTrigger("Patroling");
+        }
+        else
+        {
+            Vector2 chaseDirection = (player.position - transform.position).normalized;
+            rb.velocity = chaseDirection * chaseSpeed;
+        }
+    }
+
+    void Investigate()
+    {
         
+        Vector2 direction = (investigationTarget - (Vector2)transform.position).normalized;
+        rb.velocity = direction * patrolSpeed;
+
+        if (Vector2.Distance(transform.position, investigationTarget) < 0.2f)
+        {
+            currentState = State.Patrullando;
+            anim.SetTrigger("Patroling");
+        }
+    }
+
+    void Alert()
+    {
+        // Lógica de activación del trigger del animator
+        anim.SetTrigger("Alert");
+        timer += Time.deltaTime;
+        if (timer >= alertTime)
+        {
+            timer = 0f;
+            currentState = State.Investigando;
+            anim.SetTrigger("Investigate");        
+        }
+    }
+
+    void CheckForPlayer()
+    {
+        if (Vector2.Distance(transform.position, player.position) < visionRange)
+        {
+            currentState = State.Persiguiendo;
+            anim.SetTrigger("Chasing");
+        }
     }
 }
